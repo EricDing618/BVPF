54a15a87572c9ff307208401ec7a8d1852ca2f0e
Eric学编程 <1843794003@qq.com>
2025-08-09 20:03:24 +0800

    Update bf.py

--- src/brainfuck_without_brain/bf.py
+++ src/brainfuck_without_brain/bf.py
@@ -1,9 +1,10 @@
-# DeepSeek大手之力(85%)
-import sys
+'''最文明的一集'''
 
-class Interpreter:
+class MyInterpreter:
     def __init__(self, code:str, replace_dict:dict=None, dialect:bool=True):
         '''replace_dict格式：{方言:原指令,...}'''
+        self.change(code,replace_dict,dialect)
+    def change(self, code:str, replace_dict:dict=None, dialect=True):
         self.dialect = dialect
         self.dick = { #故意的（
             '卧': '>',
@@ -16,94 +17,104 @@ class Interpreter:
             '鼻': '[',
             '德': ']'
         } if not replace_dict else replace_dict
+        if dialect:
+            assert code==self.replaceToDialect(self.replaceToBF(code))
+        else:
+            assert code==self.replaceToBF(self.replaceToDialect(code))
         self.code = code if not dialect else self.replaceToBF(code)
     def replaceToBF(self, code:str):
         cache = []
         for c in code:
             if c.strip():
-                cache.append(self.dick.get(c,'?'))
+                cache.append(self.dick.get(c,c))
         return ''.join(cache)
     def replaceToDialect(self, code:str):
         cache = []
         for c in code:
             if c.strip():
-                cache.append(next((k for k, v in self.dick.items() if v == c), None))
+                cache.append(next((k for k, v in self.dick.items() if v == c), c))
         return ''.join(cache)
-    def optimized_brainfuck(self, input_str=""):
-        data = bytearray(30000)
-        ptr = 0
-        pc = 0
-        input_iter = iter(input_str)
-        output = []
-        
-        # 预处理：去除注释、构建跳转表
-        clean_code = [c for c in self.code if c in '><+-.,[]']
-        loop_stack = []
-        loop_pairs = {}
-        
-        for i, cmd in enumerate(clean_code):
-            if cmd == '[':
-                loop_stack.append(i)
-            elif cmd == ']':
-                if loop_stack:
-                    start = loop_stack.pop()
-                    loop_pairs[start] = i
-                    loop_pairs[i] = start
-        
-        # 合并连续相同操作
-        optimized_ops = []
-        i = 0
-        n = len(clean_code)
-        
-        while i < n:
-            cmd = clean_code[i]
-            if cmd in '><+-':
-                count = 1
-                while i + count < n and clean_code[i + count] == cmd:
-                    count += 1
-                optimized_ops.append((cmd, count))
-                i += count
+
+    def optimized_brainfuck(self):
+        '''大部分参考Copilot+https://st1020.com/a-brainfuck-language-interpreter-implemented-in-python/'''
+        bf_code = self.code
+        main_list = [0] * 30000  # 预分配30000个cell
+        now_id = 0
+        now_position = 0
+        self.output = []
+        while now_position < len(bf_code):
+            command = bf_code[now_position]
+            if command == '<':
+                if now_id == 0:
+                    print(1, '内存错误，指针位于 -1')
+                    return
+                else:
+                    now_id -= 1
+            elif command == '>':
+                now_id += 1
+                if now_id >= len(main_list):
+                    main_list.append(0)
+            elif command == '+':
+                main_list[now_id] = (main_list[now_id] + 1) % 256
+            elif command == '-':
+                main_list[now_id] = (main_list[now_id] - 1) % 256
+            elif command == '.':
+                print(chr(main_list[now_id]), end='')
+                self.output.append(chr(main_list[now_id]))
+            elif command == ',':
+                while True:
+                    input_chr = input('请输入：')
+                    if len(input_chr) == 1:
+                        if ord(input_chr) < 128:
+                            main_list[now_id] = ord(input_chr)
+                            break
+                        else:
+                            print(2, '输入错误，只能输入 ASCII 字符')
+                    else:
+                        print(2, '输入错误，只能输入一个字符')
+            elif command == '[':
+                if main_list[now_id] == 0:
+                    open_brackets = 1
+                    search_pos = now_position + 1
+                    while search_pos < len(bf_code):
+                        if bf_code[search_pos] == '[':
+                            open_brackets += 1
+                        elif bf_code[search_pos] == ']':
+                            open_brackets -= 1
+                            if open_brackets == 0:
+                                break
+                        search_pos += 1
+                    if open_brackets != 0:
+                        print(3, '代码错误，缺失：]')
+                        return
+                    now_position = search_pos
+            elif command == ']':
+                if main_list[now_id] != 0:
+                    close_brackets = 1
+                    search_pos = now_position - 1
+                    while search_pos >= 0:
+                        if bf_code[search_pos] == ']':
+                            close_brackets += 1
+                        elif bf_code[search_pos] == '[':
+                            close_brackets -= 1
+                            if close_brackets == 0:
+                                break
+                        search_pos -= 1
+                    if close_brackets != 0:
+                        print(3, '代码错误，缺失 [')
+                        return
+                    now_position = search_pos
             else:
-                optimized_ops.append((cmd, 1))
-                i += 1
-        
-        # 执行优化后的指令
-        while pc < len(optimized_ops):
-            cmd, arg = optimized_ops[pc]
-            
-            if cmd == '>':
-                ptr += arg
-                if ptr >= len(data):
-                    data.extend(bytes(len(data)))  # 双倍扩展
-            elif cmd == '<':
-                ptr -= arg
-                if ptr < 0:
-                    ptr = 0
-            elif cmd == '+':
-                data[ptr] = (data[ptr] + arg) % 256
-            elif cmd == '-':
-                data[ptr] = (data[ptr] - arg) % 256
-            elif cmd == '.':
-                output.append(chr(data[ptr]))
-                # 直接输出到stdout更高效
-                # sys.stdout.write(chr(data[ptr]))
-            elif cmd == ',':
-                try:
-                    data[ptr] = ord(next(input_iter))
-                except StopIteration:
-                    data[ptr] = 0
-            elif cmd == '[':
-                if data[ptr] == 0:
-                    pc = loop_pairs[pc]
-            elif cmd == ']':
-                if data[ptr] != 0:
-                    pc = loop_pairs[pc]
-            
-            pc += 1
-        
-        return ''.join(output)
+                print(0, '无法解析的字符：' + command)
+            now_position += 1
+        # print(main_list)  # 不再输出内存
     
 if __name__ == "__main__":
-    bf = Interpreter('泥泥泥泥泥泥泥泥鼻卧泥泥泥泥鼻卧泥泥卧泥泥泥卧泥泥泥卧泥草草草草马德卧泥卧泥卧马卧卧泥鼻草德草马德卧卧格卧马马马格泥泥泥泥泥泥泥格格泥泥泥格卧卧格草马格草格泥泥泥格马马马马马马格马马马马马马马马格卧卧泥格卧泥泥格')
+    bf = MyInterpreter('++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.',dialect=False)
     print(bf.replaceToDialect("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."))
-    print(bf.optimized_brainfuck())
+    print(bf.replaceToBF("泥泥泥泥泥泥泥泥鼻卧泥泥泥泥鼻卧泥泥卧泥泥泥卧泥泥泥卧泥草草草草马德卧泥卧泥卧马卧卧泥鼻草德草马德卧卧格卧马马马格泥泥泥泥泥泥泥格格泥泥泥格卧卧格草马格草格泥泥泥格马马马马马马格马马马马马马马马格卧卧泥格卧泥泥格"))
+    bf.optimized_brainfuck()
+    bf.change('++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.', dialect=False)
+    bf.optimized_brainfuck()
+    bf.change('泥泥泥泥泥泥泥泥鼻卧泥泥泥泥鼻卧泥泥卧泥泥泥卧泥泥泥卧泥草草草草马德卧泥卧泥卧马卧卧泥鼻草德草马德卧卧格卧马马马格泥泥泥泥泥泥泥格格泥泥泥格卧卧格草马格草格泥泥泥格马马马马马马格马马马马马马马马格卧卧泥格卧泥泥格')
+    bf.optimized_brainfuck()
