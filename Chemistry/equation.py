af66f132b8fa8838cf70f6d736d369e9acd1c2f3
Eric学编程 <1843794003@qq.com>
2026-02-05 21:24:46 +0800

    hot shit bug fly

--- Chemistry/equation.py
+++ Chemistry/equation.py
@@ -14,6 +14,38 @@ class Compound:
         if self.name[-1] in ('↑','↓'):
             self.name = self.name[:-1]
     def transform(self):
+        current_symbol = ""
+        current_num = ""
+        if not self.name[0].isupper():
+            raise SyntaxError("Invalid Compound")
+        for s in self.name:
+            if s.isupper():
+                if len(list(filter(str.isupper,current_symbol))) > 1 or (current_symbol and not current_symbol[0].isupper()):
+                    raise SyntaxError("Invalid Compound")
+                
+                if current_num:
+                    self.elements.append(Element(current_symbol,int(current_num)))
+                else:
+                    self.elements.append(Element(current_symbol))
+                current_num = ""
+                current_symbol = s
+            elif s.isdigit():
+                if len(list(filter(str.isupper,current_symbol))) > 1 or not current_symbol[0].isupper():
+                    raise SyntaxError("Invalid Compound")
+                current_num+=s
+            elif s.islower():
+                if len(list(filter(str.isupper,current_symbol))) > 1 or not current_symbol[0].isupper():
+                    raise SyntaxError("Invalid Compound")
+                current_symbol+=s
+            else:
+                raise SyntaxError("Invalid Compound")
+            
+            if current_num:
+                self.elements.append(Element(current_symbol,int(current_num)))
+            else:
+                self.elements.append(Element(current_symbol))
+            
+    def __transform(self):
         next_element = False
         current_symbol = ""
 
